c7f752bf70fba165e8ce44d813cce75f73784609
karam-qaoud <karamqaoud@gmail.com>
2020-08-02 19:03:57 +0300

    Extra edits on sign up && new sig in component relates to #11

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './pages/signup/index'
+import LogIn from './pages/logIn/index'
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+      {/* <SignUp/> */}
+      {/* <LogIn /> */}
     </div>
   );
 }
