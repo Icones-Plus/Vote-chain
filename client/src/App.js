c5a84b1db8bec150b5abf33f7a744e1e355150d3
ahmad1091 <ahmadtayeb91@gmail.com>
2020-08-03 10:28:28 +0300

    delete the packagelock and add adding  .prettierrc

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SignUp from './pages/signup/index'
-import LogIn from './pages/logIn/index'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SignUp from "./pages/signup/index";
+import LogIn from "./pages/logIn/index";
 function App() {
   return (
     <div className="App">
