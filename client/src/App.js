8c6edb2227a938a766d049ef4a7c72d5fa1355f6
karam-qaoud <karamqaoud@gmail.com>
2020-08-03 13:57:21 +0300

    Navbar component relates to #18

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import SignUp from './components/signup/index'
 import LogIn from './components/logIn/index'
+import Navbar from './components/navbar/index'
 function App() {
   return (
     <div className="App">
       {/* <SignUp/> */}
       {/* <LogIn /> */}
+      {/* <Navbar /> */}
     </div>
   );
 }
