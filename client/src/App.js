1822e2c09f25f6a7156aaf0ffa14a26e932efeb0
AymanElmanas <ayye942@gmail.com>
2020-08-04 17:30:39 +0300

    landingPageV1

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import SignUp from './components/signup/index'
-import LogIn from './components/logIn/index'
-import Navbar from './components/navbar/index'
+import React from "react";
+import "./App.css";
+import LandingPage from "./pages/landingPage/index"
+import SignUp from "./components/signup/index";
+import LogIn from "./components/logIn/index";
 function App() {
   return (
     <div className="App">
       {/* <SignUp/> */}
       {/* <LogIn /> */}
-      {/* <Navbar /> */}
+      <LandingPage/>
     </div>
   );
 }
