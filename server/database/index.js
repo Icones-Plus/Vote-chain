02029d7c6ce7ee8d4530f6fbbaec2665daadf3da
Yasmin Hillis <jasminehillis94@gmail.com>
2020-08-04 17:55:57 +0300

    add signup

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 let connection = mongoose
-  .connect("mongodb://localhost:27017/voting", { useNewUrlParser: true })
+  .connect("mongodb://localhost:27017/votedb", { useNewUrlParser: true })
   .then(() => {
     console.log("connected to dataBase");
   })
@@ -8,23 +9,43 @@ let connection = mongoose
     console.log(err);
   });
 
-let userSchema = mongoose.Schema({
+let userSchema = new mongoose.Schema({
   mobile: Number,
-  motherName: String,
+  mother_name: String,
+  gender: String,
   id: Number,
-  Email: String,
+  email: String,
+  first_name: String,
+  last_name: String,
   password: String,
-  dateOfBirth: Number,
+  dateOfBirth: Date,
   voted: Boolean,
 });
+
+userSchema.methods.generateHash = function(password){
+  console.log('here');
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
+userSchema.methods.comparePassword = function(password){
+  return bcrypt.compareSync(password, this.password)
+}
+
 
 let cndidateSchema = mongoose.Schema({
   name: String,
   info: String,
 });
 
-let userModel = mongoose.model("newUser", userSchema);
 
+
+let userModel = mongoose.model("newUser", userSchema);
+// var newEntity = new userModel({"id":"5154778472","mother_name":"Alane","mobile":993111980,"email":"acoupland0@ameblo.jp","gender":"Female","voted":false,"dateOfBirth":"11/13/2019","password":null,"first_name":"Ammamaria","last_name":"Coupland"});
+ // newEntity.save().then((res)=>{
+ //   console.log('dine');
+ // }).catch((err)=>{
+ //   console.log('EEE',err);
+ // })
 let candidateModel = mongoose.model("newCandidate", cndidateSchema);
 
 module.exports.userModel = userModel;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var path = require("path");
 const db = require("./database/index");
+const routes = require('./controller/index');
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(express.json())
+
+app.use('/', routes);
+// app.use(bodyParser.json())
+// app.get('/', (req, res)=> {
+//   res.send('hiiiiiiii')
+// })
 
 app.use(express.static(path.join(__dirname, "../client/public")));
 
