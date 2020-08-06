ce588b88ddca3b65cb3a416f4eec96c398764b17
karam-qaoud <karamqaoud@gmail.com>
2020-08-06 14:13:48 +0300

    Clean components + Ready sign in and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
 import "./App.css";
 import LandingPage from "./pages/landingPage/index"
-import SignUp from "./components/signup/index";
-import SignIn from "./components/signIn/index";
-import Routing from "./components/router/index"
 function App() {
   return (
     <div className="App">
-      {/* <header> <Navbar /></header>
-      <Routing /> */}
-      {/* <SignUp/> */}
-      {/* <SignIn /> */}
-      <Routing/>
       <LandingPage/>
     </div>
   );
