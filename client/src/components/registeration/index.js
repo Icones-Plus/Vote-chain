865c9efcb7de04548511a96ab3af6cd056114efc
karam-qaoud <karamqaoud@gmail.com>
2020-08-07 16:30:46 +0300

    modifiying registeration and connecting to backend

--- client/src/components/registeration/index.js
+++ client/src/components/registeration/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 class SignIn extends React.Component {
   state = { id: '', password: '', component: null };
   handleChange = this.handleChange.bind(this);
@@ -47,8 +48,8 @@ class SignIn extends React.Component {
               />
               <br />
               <br />
-              <button type="submit" value="Log in" className="btn-custom"> Sign In </button>
-              <p>Don't have an account?</p><button className="button" onClick={this.showSignupForm.bind(this)} >Sign up</button>;
+              <button type="submit" value="Log in" className="btn-custom"> Sign In </button><br /><br />
+              <p>Don't have an account? <button className="button" onClick={this.showSignupForm.bind(this)} >Sign up</button></p>;
               </form>
           </div>
         </div>
@@ -60,7 +61,7 @@ class SignIn extends React.Component {
 
 
 class SignUp extends React.Component {
-  state = { id: '', email: '', phoneNumber: '', dateOfBirth: '', motherName: '', signIn: null };
+  state = { id: '', first_name: '', last_name:'', email: '', phoneNumber: '', dateOfBirth: '', gender: '', motherName: '', signIn: null };
   handleChange = this.handleChange.bind(this);
   handleSubmit = this.handleSubmit.bind(this);
   handleChange(event) {
@@ -68,8 +69,24 @@ class SignUp extends React.Component {
   };
   handleSubmit(event) {
     event.preventDefault();
-    this.setState({ id: '', email: '', phoneNumber: '', dateOfBirth: '', motherName: '' })
+    this.setState({ id: '', first_name: '', last_name: '', email: '', phoneNumber: '', dateOfBirth: '', gender: '', motherName: '' })
     //Axios request goes here
+    axios.post('/', {
+      mobile: this.state.phoneNumber,
+      mother_name: this.state.motherName,
+      gender: this.state.gender,
+      id: this.state.id,
+      email: this.state.email,
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      dateOfBirth: this.state.dateOfBirth
+    })
+    .then(function (response) {
+      console.log("Sign up data got sent", response);
+    })
+    .catch(function (error) {
+      console.log("Failed to send sign up data", error);
+    });
   };
   showSignin() {
     this.setState({
@@ -87,7 +104,15 @@ class SignUp extends React.Component {
             <label>
               ID
             <input type="number" name="id" placeholder=' e.g. 403328682' value={this.state.id} onChange={this.handleChange} />
-            </label>
+              </label><br /><br />
+              <label>
+                First Name
+            <input type="text" name="first_name" placeholder=' John ' value={this.state.first_name} onChange={this.handleChange} />
+              </label><br /><br />
+              <label>
+                Last Name
+            <input type="text" name="last_name" placeholder=' Smith ' value={this.state.last_name} onChange={this.handleChange} />
+              </label>
             <label><br /><br />
             Email
           <input type="email" name='email' placeholder=' e.g. jsmith@gmail.com' value={this.state.email} onChange={this.handleChange} />
@@ -99,13 +124,21 @@ class SignUp extends React.Component {
             <label>
               Date Of Birth
             <input type="date" name='dateOfBirth' value={this.state.dateOfBirth} onChange={this.handleChange} />
-            </label> <br /><br />
+              </label> <br /><br />
+              <label>
+                Gender
+                <select name='gender' value={this.state.gender} onChange={this.handleChange} >
+                  <option value="none"> </option>
+                  <option value="male"> Male </option>
+                  <option value="female"> Female </option>
+                </select>
+              </label> <br /><br />
             <label>
               Mother Name
             <input type="text" name='motherName' placeholder=' e.g. Huda' value={this.state.motherName} onChange={this.handleChange} />
             </label><br /><br />
-            <input type="submit" value="Sign up" />
-            <p>Go bak to </p> <button className="button" onClick={this.showSignin.bind(this)}>Sign in</button>
+              <input type="submit" value="Sign up" /><br /><br />
+            <p>Go bak to <button className="button" onClick={this.showSignin.bind(this)}>Sign in</button></p> 
           </form>
         </div>
         </div>
--- server/server.js
+++ server/server.js
@@ -10,4 +10,10 @@ app.use(express.json());
 app.use("/", routes);
 app.use(express.static(path.join(__dirname, "../client/public")));
 
+
+
+app.post('/#SignIn', function (req, res) {
+    console.log("======================================> Yo I am here")
+    res.send('hello world')
+})
 module.exports = app;
