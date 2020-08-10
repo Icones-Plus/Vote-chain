b10ba9b7cc68d7a92c8759ddde65bb09d4deb549
karam-qaoud <karamqaoud@gmail.com>
2020-08-10 17:22:31 +0300

    Feedback component ready

--- client/src/components/admin/feedback.jsx
+++ client/src/components/admin/feedback.jsx
@@ -1,46 +1,46 @@
-import React, { Component } from "react";
-import axios from 'axios';
+import React, {Component} from "react";
+import axios from "axios";
 
 class Feedback extends React.Component {
-
   state = {
     data: null,
-    count: 0
-  }
+    count: 0,
+  };
   componentDidMount() {
-    axios.get("/contact")
-      .then(result => {
-        console.log("Success on sending get request", result)
-        result.data.map(elem => {
-          this.state.count++;
-          this.setState({
-            data:
-            <tr>
-              <td>{this.state.count}</td>
-              <td>{elem.name}</td>
-              <td>{elem.email}</td>
-              <td>{elem.message}</td>
-            </tr> 
-          });
-        })
+    axios
+      .get("/contact")
+      .then((result) => {
+        console.log("Success on sending get request", result.data);
+        this.setState({data: result.data});
       })
-      .catch(err => {
-      console.log("Error in retrieving data on get request", err)
-    })
+      .catch((err) => {
+        console.log("Error in retrieving data on get request", err);
+      });
   }
 
   render() {
+    var data = this.state.data || [];
     return (
       <div id="feedback">
         <h1> Feedback</h1>
         <table style={{width: "100%"}}>
           <tr>
-            <th style={{padding:"0px"}}>number</th>
+            <th style={{padding: "0px"}}>number</th>
             <th>Name</th>
             <th>Email</th>
             <th>Message/concern</th>
           </tr>
-          {this.state.data}
+          {data.map((elem) => {
+            this.state.count++;
+            return (
+              <tr>
+                <td>{this.state.count}</td>
+                <td>{elem.name}</td>
+                <td>{elem.email}</td>
+                <td>{elem.message}</td>
+              </tr>
+            );
+          })}
         </table>
       </div>
     );
