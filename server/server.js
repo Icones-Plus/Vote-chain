da2880bee810ec33257a2fb3db3733f761500c38
Ahmad Abdallah <38608782+ahmad1091@users.noreply.github.com>
2020-08-10 17:46:48 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const bodyParser = require("body-parser");
 var cors = require("cors");
 
 var app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/build")));
