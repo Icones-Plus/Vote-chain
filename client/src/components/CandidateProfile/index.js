c60b59814517cf6af89bb47308c62828edc976be
Ahmad Abdallah <38608782+ahmad1091@users.noreply.github.com>
2020-08-19 13:26:36 +0300

    Update index.js

--- client/src/components/CandidateProfile/index.js
+++ client/src/components/CandidateProfile/index.js
@@ -28,34 +28,3 @@ function CandidateProfile(props) {
   );
 }
 export default CandidateProfile;
-
-// <div>
-//   <div class="bor">
-//     <img src={data[2]}></img>
-//   </div>
-//   <div class="div2">{data[0]}</div>
-//   <div class="div3"></div>
-//   <div class="div4">{data[1]}</div>
-// </div>
-// // <div className="test-parent">
-// //   <div className="sidebar">test from side bar</div>
-// //   <div className="content">test from content</div>
-// // </div>
-
-// <div>
-//   <section id="packages">
-//     <div class="box">
-//       <img src="img/garden.jpg"></img>
-//     </div>
-//     <div class="box">
-//       <div class="content">
-//         <div>
-//           <h2>OCEAN VIEW</h2>
-//           <p>Marvellous ocean views.</p>
-//           <a href="oceanView.html">Read More</a>
-//         </div>
-//       </div>
-//       <img src="img/ocean.jpg"></img>
-//     </div>
-//   </section>
-// </div>
