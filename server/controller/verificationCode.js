b38ab088b4f1f1c8c0eecc22a50531fa19b65a0b
Ahmad Abdallah <38608782+ahmad1091@users.noreply.github.com>
2020-08-21 23:37:51 +0300

    Update verificationCode.js

--- server/controller/verificationCode.js
+++ server/controller/verificationCode.js
@@ -40,6 +40,7 @@ exports.verfiy = function (req, res) {
       res.status(401).json("Error: server error");
     } else {
       var code = token.slice(29, 36);
+       console.log("My generated code ...........", code);
       client.sms.message(
         messageCallback,
         process.env.phoneNumber,
